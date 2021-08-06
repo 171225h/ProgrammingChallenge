17aa26990419f287fb362a98967660c93c565c37
은영 <e_yeong@naver.com>
2021-08-06 17:19:59 +0900

    9465
    Using DP

--- dynamicProgramming/Sticker.java
+++ dynamicProgramming/Sticker.java
@@ -1,72 +1,80 @@
 package dynamicProgramming;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Sticker {
-
-	// 9465번 	2021.02.22.
-
+	
+	// 9465번 2021.08.06.
+	
 	/*
-	 * 선택에 어떤 규칙이 있는 것까진 알아냈지만
-	 * 점화식 만들기를 실패함
+	 * 5달 전에 풀지 못한 DP문제를 드디어 풀었다 ㅜ.ㅠ 감동실화
+	 * 
 	 */
 
-	public static void main(String[] args) throws Exception{
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-		int testCase = Integer.parseInt(br.readLine());
-		int n=0;
-		int[][] arr=null;
 		StringBuilder sb = new StringBuilder();
+		
+		int testCase = Integer.parseInt(br.readLine());
 		StringTokenizer st = null;
-
+		int n = 0;
+		int[][] sticker = null;
+		int[][] dp = null;
+		int count = 0;
+		
 		for(int i = 0; i<testCase; i++) {
 			n = Integer.parseInt(br.readLine());
-			arr = new int[2][n];
-
-			// 각 testCase input
+			sticker = new int[2][n];
+			dp = new int[2][n];
+			
+			count = 0;
+			
 			for(int j = 0; j<2; j++) {
 				st = new StringTokenizer(br.readLine());
 				for(int k = 0; k<n; k++) 
-					arr[j][k]=Integer.parseInt(st.nextToken());
-
+					sticker[j][k] = Integer.parseInt(st.nextToken());
 			}
-
-			//dp
-			if(n==1)
-				sb.append(Math.max(arr[0][0], arr[0][1])+"");
-			else if(n==2)
-				sb.append(Math.max(arr[0][0]+arr[1][1], arr[0][1]+arr[1][0]));
-			else {
-				// 답
-				arr[0][1] += arr[1][0];
-				arr[1][1] += arr[0][0];
+			
+			//showSticker(sticker);
 				
-				for(int j = 2; j<n; j++) {
-					// 처음 비슷하게 생각해낸 것
-					//arr[0][j] += Math.max(arr[0][j-2]+arr[1][j-1], arr[1][j-2]);
-					//arr[1][j] += Math.max(arr[1][j-2]+arr[0][j-1], arr[0][j-2]);
-				
-					// 답
-					arr[0][j] += Math.max(arr[1][j-1], arr[1][j-2]);
-					arr[1][j] += Math.max(arr[0][j-1], arr[0][j-2]);
-				}
-
-				sb.append(Math.max(arr[0][n-1], arr[1][n-1]));
+			dp[0][0] = sticker[0][0];
+			dp[1][0] = sticker[1][0];
+			
+			
+			if(n>1) {
+				dp[0][1] = dp[1][0]+sticker[0][1];
+				dp[1][1] = dp[0][0]+sticker[1][1];
 			}
+			
+			for(int j = 2; j<n; j++) {
+				dp[0][j] = sticker[0][j] + Math.max(dp[0][j-2], Math.max(dp[1][j-2], dp[1][j-1]));
+				dp[1][j] = sticker[1][j] + Math.max(dp[0][j-2], Math.max(dp[1][j-2], dp[0][j-1]));
+			}
+			
+			for(int j = 0; j<2; j++) {
+				for(int k = 0; k<sticker[0].length; k++) 
+					count = Math.max(count, dp[j][k]);
+			}
+			
+			sb.append(count+"\n");
+			
+		}
+		
+		sb.delete(sb.length()-1, sb.length());
+		System.out.print(sb.toString());
+	}
 
-			if(i<testCase-1)
-				sb.append("\n");
+	private static void showSticker(int[][] sticker) {
+		
+		for(int j = 0; j<2; j++) {
+			for(int k = 0; k<sticker[0].length; k++) 
+				System.out.print(sticker[j][k]+" ");
+			System.out.println();
 		}
 
-		bw.write(sb.toString());
-		bw.flush();
-		bw.close();
 	}
 
 }
